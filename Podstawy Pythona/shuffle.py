309f14949379f0aaef42a42caf6479a379c1cd8b
slach12 <slach12@wp.pl>
2023-08-23 21:44:33 +0200

    Create shuffle.py

---
+++ Podstawy Pythona/shuffle.py
@@ -0,0 +1,15 @@
+cardList = [ "9", "9", "9", "9",
+             "10", "10", "10", "10",
+             "Jack", "Jack", "Jack", "Jack",
+             "King", "King", "King", "King",
+             "Queen", "Queen", "Queen", "Queen"
+             "Ace", "Ace", "Ace", "Ace",
+             "Joker", "Joker"
+           ]
+
+
+import random
+
+random.shuffle(cardList)
+
+print(cardList)
