e7fcb11ad3e991ffe008fded28be59be580fd639
slach12 <slach12@wp.pl>
2023-09-27 01:51:59 +0200

    Create json_data_format.py

---
+++ Podstawy Pythona/json_data_format.py
@@ -0,0 +1,19 @@
+import json
+
+film = {
+    "title" : "Ale ja nie będę tego robił!",
+    "release_year" : 1969,
+    "won_oscar" : True,
+    "actors": ("Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"),
+    "budget" : None,
+    "credits" : {
+            "director" : "Arkadiusz Włodarczyk",
+            "writer" : "Alan Burger",
+            "animator" : "Anime Animatrix"
+            }
+}
+
+encodingFilm = json.dumps(film, ensure_ascii =False)
+
+with open("sample.json", "w", encoding = "UTF-8") as file:
+    json.dump(film,file, ensure_ascii = False)
