d56f165291297e3f1b3a24e36d132c6198ebef3e
slach12 <slach12@wp.pl>
2023-09-28 00:29:42 +0200

    Update json_data_format.py

--- Podstawy Pythona/json_data_format.py
+++ Podstawy Pythona/json_data_format.py
@@ -17,3 +17,15 @@ encodingFilm = json.dumps(film, ensure_ascii =False)
 
 with open("sample.json", "w", encoding = "UTF-8") as file:
     json.dump(film,file, ensure_ascii = False)
+
+
+encodedRetrievedMovie = '{"title": "Ale ja nie będę tego robił!", "release_year": 1969, "won_oscar": true, "actors": ["Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"], "budget": null, "credits": {"director": "Arkadiusz Włodarczyk", "writer": "Alan Burger", "animator": "Anime Animatrix"}}'
+
+decodedMovie = json.loads(encodedRetrievedMovie )
+
+
+with open("sample.json",encoding="UTF-8") as file :
+    wynik = json.load(file)
+
+print("wynik")
+print(wynik)
