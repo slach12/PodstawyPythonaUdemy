99784beee23f5297534dc0e4be16d7b6f23b0fc2
slach12 <slach12@wp.pl>
2023-10-02 22:43:28 +0200

    Update json_data_format.py

--- Podstawy Pythona/json_data_format.py
+++ Podstawy Pythona/json_data_format.py
@@ -13,12 +13,12 @@ film = {
             }
 }
 
-encodingFilm = json.dumps(film, ensure_ascii =False)
+encodingFilm = json.dumps(film, ensure_ascii =False, indent = 4, sort_keys= True)
 
 with open("sample.json", "w", encoding = "UTF-8") as file:
     json.dump(film,file, ensure_ascii = False)
 
-
+'''
 encodedRetrievedMovie = '{"title": "Ale ja nie będę tego robił!", "release_year": 1969, "won_oscar": true, "actors": ["Arkadiusz Włodarczyk", "Wiolleta Włodarczyk"], "budget": null, "credits": {"director": "Arkadiusz Włodarczyk", "writer": "Alan Burger", "animator": "Anime Animatrix"}}'
 
 decodedMovie = json.loads(encodedRetrievedMovie )
@@ -29,3 +29,16 @@ with open("sample.json",encoding="UTF-8") as file :
 
 print("wynik")
 print(wynik)
+'''
+
+print(encodingFilm)
+
+with open("sample.json", encoding="UTF-8") as file:
+    wynik = json.load(file)
+
+print(wynik)
+
+
+import  pprint
+
+pprint.pprint(wynik)
