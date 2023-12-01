83ffaa4c2c3630b919549bac6f389a342c076d47
slach12 <slach12@wp.pl>
2023-12-01 03:10:52 +0100

    Lekcja 92
    Lekcja 92

--- Podstawy Pythona/jsonplaceholder.py
+++ Podstawy Pythona/jsonplaceholder.py
@@ -3,6 +3,35 @@ import json
 
 r = requests.get("https://jsonplaceholder.typicode.com/todos")
 
+
+def count_Task_Frequency(tasks):
+    completedTaskFrequencyByUser = dict()
+    for entry in tasks:
+        #print(entry)
+        if (entry["completed"] == True):
+            try:
+                completedTaskFrequencyByUser[entry["userId"]]+=1
+            except KeyError:
+                completedTaskFrequencyByUser[entry["userId"]] = 1
+
+    return completedTaskFrequencyByUser
+
+
+def get_keys_with_top_value(my_dict):
+    return [
+        key
+        for (key,value)in my_dict.items()
+        if value == max(my_dict.values())
+    ]
+
+def users_with_top_completed_tasks(completedTaskFrequencyByUser):
+    userIdWithMaxCompletedAmountOfTask =[]
+    maxAMountOfCompletedTask = max(completedTaskFrequencyByUser.values())
+    for userId, numberOfCompletedTask in completedTaskFrequencyByUser.items():
+        if(numberOfCompletedTask == maxAMountOfCompletedTask):
+            userIdWithMaxCompletedAmountOfTask.append(userId)
+    return userIdWithMaxCompletedAmountOfTask
+
 #print(r.text)
 
 #tasks = json.loads(r.text)
@@ -11,5 +40,10 @@ try:
 except json.JSONDecodeError:
     print("Niepoprawny format")
 else:
-    print("Wszystko jest ok.")
-    print(tasks[0])
+    #print("Wszystko jest ok.")
+    #print(tasks[0])
+    completedTaskFrequencyByUser = count_Task_Frequency(tasks)
+    userWithTopCompletedTasks = users_with_top_completed_tasks(completedTaskFrequencyByUser)
+    print("Wręczamy ciasteczko mistrzunia do użytkowników o id:",userWithTopCompletedTasks)
+
+ 
