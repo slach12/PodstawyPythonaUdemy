e569bc859c202fa1aa895222428326d953a112c3
slach12 <slach12@wp.pl>
2023-12-08 02:01:55 +0100

    Lesson 95 and 96

--- Podstawy Pythona/jsonplaceholder.py
+++ Podstawy Pythona/jsonplaceholder.py
@@ -44,8 +44,12 @@ else:
     #print(tasks[0])
     completedTaskFrequencyByUser = count_Task_Frequency(tasks)
     userWithTopCompletedTasks = users_with_top_completed_tasks(completedTaskFrequencyByUser)
-    print("Wręczamy ciasteczko mistrzunia do użytkowników o id:",userWithTopCompletedTasks)
+#    print("Wręczamy ciasteczko mistrzunia do użytkowników o id:",userWithTopCompletedTasks)
 
+
+
+#Sposób pierwszy
+'''
 
 r = requests.get("https://jsonplaceholder.typicode.com/users")
 
@@ -55,6 +59,39 @@ for user in users:
     if user["id"] in userWithTopCompletedTasks:
         print("Wręczamy ciasteczko mistrzunia do użytkownika o imieniu :",user["name"])
         
-    
+''' 
 
- 
+#Sposób drugi
+'''
+print("Sposób drugii")
+
+for userId in userWithTopCompletedTasks:
+    #print(userId)
+    #r = requests.get("https://jsonplaceholder.typicode.com/users/"+str(userId))
+    r = requests.get("https://jsonplaceholder.typicode.com/users/",params = "id="+str(userId))
+    user = r.json()
+    #   print(user[0]["id"])
+    print("Wręczamy ciasteczko mistrzunia do użytkownika o imieniu :",user[0]["name"])
+'''
+
+#Sposób trzeci
+
+def change_list_into_conj_of_param(my_list, key="id"):
+    conj_param = key + "="
+    last_iteration = len(my_list)
+    i=0
+    for item in my_list:
+        i+=1
+        if(i == last_iteration):
+            conj_param+=str(item) 
+        else:
+            conj_param+=str(item) + "&"+key+"="
+
+    return conj_param
+
+#conj_param=change_list_into_conj_of_param(userWithTopCompletedTasks)
+conj_param=change_list_into_conj_of_param([2,4,7])
+r = requests.get("https://jsonplaceholder.typicode.com/users/",params = conj_param)#+str(userId))
+users = r.json()
+for user in users:
+    print("Wręczamy ciasteczko mistrzunia do użytkownika o imieniu :",user["name"])
