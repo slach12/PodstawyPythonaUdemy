62a6b0350e3fbc2eb772253cbcbcdcce59e68b13
slach12 <slach12@wp.pl>
2024-01-04 15:44:31 +0100

    Lekcja 99 i 100
    Lekcja 99 i 100 otwieranie zakładek z pytaniami z stackoverflow

--- Podstawy Pythona/przetwarzanie.py
+++ Podstawy Pythona/przetwarzanie.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pprint
+import webbrowser
 
 params = {
            "site" : "stackoverflow",
@@ -16,7 +17,11 @@ r = requests.get("https://api.stackexchange.com/2.2/questions/", params)
 
 try:
     questions = r.json()
-except json.decoder.JSONDecodeError :
+except json.decoder.JSONDecodeError:
     print("Niepoprawny format")
 else:
-    pprint.pprint(questions )
+   # pprint.pprint(questions)
+    for question in questions["items"]:
+        webbrowser.open_new_tab(question["link"])
+        #pprint.pprint(question["link"])
+        
