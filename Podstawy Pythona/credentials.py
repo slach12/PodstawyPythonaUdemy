48a4fe8f27c0eaf71d13beb2ee3b055a49e3f273
slach12 <slach12@wp.pl>
2024-01-13 01:39:32 +0100

    Lekcja 105
    105. Logowanie przez header - nagłówek

---
+++ Podstawy Pythona/credentials.py
@@ -0,0 +1,3 @@
+headers = {
+        'x-api-key' : 'live_xW3psnIxgaCKqgk3E6Ha0jdeK6yTqMWzHq9VnudU7Nllsmbu7GDBo23trSSFFfuk'
+    }
---
+++ Podstawy Pythona/logowanieheader.py
@@ -0,0 +1,38 @@
+import requests
+import json
+from pprint import pprint
+import webbrowser
+from datetime import datetime, timedelta
+import credentials as cr
+
+#print(datetime.today())
+#print(timedelta(days=7))
+
+timeBefore = timedelta(days =10)
+searchDate = datetime.today() - timeBefore
+
+
+'''
+params = {
+           "api_key" : "mUKzVG9C1VNMHZ1TqmG9eF6oPsC20Vgx",
+           "country" : "pl",
+           "year" : 2023,
+           "month" : 12
+        }
+'''
+
+
+
+r = requests.get("https://api.thecatapi.com/v1/favourites", headers= cr.headers)
+
+
+
+
+
+
+try:
+    content = r.json()
+except json.decoder.JSONDecodeError:
+    print("Niepoprawny format")
+else:
+     pprint(content)
